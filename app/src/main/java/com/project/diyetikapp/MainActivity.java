ddb17da391aef728bdbc63f949d3e26e5da1d78e
tugbaozaydin <kstebekboku06>
2019-08-14 02:35:00 +0300

    Inıtıal Commit

---
+++ app/src/main/java/com/project/diyetikapp/MainActivity.java
@@ -0,0 +1,15 @@
+package com.project.diyetikapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.project.diyetikapp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
