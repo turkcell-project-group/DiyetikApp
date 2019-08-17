36d5a9e0e38a59fd8c400673f6a6f4cff2603cce
pelvinaydn <pelvin16@gmail.com>
2019-08-17 22:00:11 +0300

    message

---
+++ app/src/main/java/com/project/diyetikapp/SignIn.java
@@ -0,0 +1,76 @@
+package com.project.diyetikapp;
+
+import android.app.ProgressDialog;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class SignIn extends AppCompatActivity {
+    EditText edtPhone,edtPassword;
+    Button btnSingIn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+
+        edtPassword =(MaterialEditText)findViewById(R.id.edtPassword);
+        edtPhone =(MaterialEditText)findViewById(R.id.edtPhone);
+        btnSingIn = (Button)findViewById(R.id.btnSignIn);
+
+
+        //Init firebase
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        final DatabaseReference table_user = database.getReference("User");
+
+        btnSingIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final ProgressDialog mDialog = new ProgressDialog(SignIn.this);
+                mDialog.setMessage("Please waiting...");
+                mDialog.show();
+
+                table_user.addValueEventListener(new ValueEventListener() {
+
+
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        //Check if user not exist in database
+                        if(dataSnapshot.child(edtPhone.getText().toString()).exists()){
+
+
+                            // Get user information
+                            mDialog.dismiss();
+
+                            User user = dataSnapshot.child(edtPhone.getText().toString()).getValue(User.class);
+                            if(user.getPassword().equals(edtPassword.getText().toString())){
+                                Toast.makeText(SignIn.this,"Sing in successfully!",Toast.LENGTH_SHORT).show();
+
+                            } else {
+                                Toast.makeText(SignIn.this, "Sing in failed!", Toast.LENGTH_SHORT).show();
+
+                            } } else{
+                            mDialog.dismiss();
+                            Toast.makeText(SignIn.this, "User not exist in Database", Toast.LENGTH_SHORT).show();
+
+
+                        }
+                    }
+
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+
+            }
+        });
+
+
+
+    }
+}
+
