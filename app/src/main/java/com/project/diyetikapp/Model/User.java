2d9d845fab0f29ea920ef80cc265b8005e66e330
tugbaozaydin <kstebekboku06>
2019-08-25 23:54:59 +0300

    user IsStaff added

--- app/src/main/java/com/project/diyetikapp/Model/User.java
+++ app/src/main/java/com/project/diyetikapp/Model/User.java
@@ -4,6 +4,7 @@ public class User{
     private  String Name;
     private String Password;
     private String Phone;
+    private  String IsStaff;
 
     public User() {
     }
@@ -11,7 +12,16 @@ public class User{
     public User(String name, String password ) {
         Name = name;
         Password = password;
+        IsStaff = "false";
 
+    }
+
+    public String getIsStaff() {
+        return IsStaff;
+    }
+
+    public void setIsStaff(String isStaff) {
+        IsStaff = isStaff;
     }
 
     public String getPhone() {
